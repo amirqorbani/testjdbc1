b80b526de7932100f2f12e7f7d3ca7b0dd6f0f52
amirghorbani <amirghorbanali2@gmail.com>
2023-05-26 11:41:28 +0430

    user repository updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        Statement stmt =
 
     }
 }
--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -1,7 +1,19 @@
 package org.example.repository;
 
-import java.sql.Statement;
+import org.example.connection.MyConnection;
+import org.example.entity.User;
+
+import java.sql.*;
 
 public class UserRepository {
+    public void save(User user) throws SQLException {
+        Connection connection = MyConnection.getConnections();
+        String sql ="INSERT INTO USER (user_id , username , password , signup_date) VALUES (? , ? , ? , ?) ";
+                PreparedStatement pstmt = connection.prepareStatement(sql);
+                pstmt.setInt(1 , user.getUser_id());
+                pstmt.setString(2 , user.getUsername());
+                pstmt.setString(3 , user.getPassword() );
+                pstmt.setDate(4 , (Date) user.getSignup_date());
 
+    }
 }
